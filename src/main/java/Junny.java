462b40f1ce25c04e9d253e1d1edf5001841834ac
junrong <E1354441@u.nus.edu>
2025-08-21 00:36:21 +0800

    Level-3: Mark as Done

--- src/main/java/Junny.java
+++ src/main/java/Junny.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Junny {
     public static void main(String[] args) {
-        String[] list_of_act = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
         Scanner scanner = new Scanner(System.in);
         printLine();
@@ -11,22 +11,27 @@ public class Junny {
         printLine();
 
         while (true) {
-            String user_input = scanner.nextLine();
-            if (user_input.equals("bye")) {
-                System.out.println(" Bye. Hope to see you again soon!");
-                printLine();
+            String userInput = scanner.nextLine();
+            String[] inputByParts = userInput.split(" ");
+            if (userInput.equals("bye")) {
+                printBye();
                 break;
-            } else if (user_input.equals("list")) {
-                printLine();
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + "." + list_of_act[i]);
-                }
-                printLine();
-            } else {
-                list_of_act[count] = user_input;
+            } else if (userInput.equals("list")) {
+                printAllTasks(tasks, count);
+            } else if (inputByParts[0].equals("mark")) {
+                int index = Integer.parseInt(inputByParts[1]) - 1;
+                // VERY IMPORTANT: mark 2 extract 2, but it actually mark tasks[1]!!!
+                markDone(tasks[index]);
+            } else if (inputByParts[0].equals("unmark")) {
+                int index = Integer.parseInt(inputByParts[1]) - 1;
+                markUndone(tasks[index]);
+            }
+            else {
+                Task t = new Task(userInput);
+                tasks[count] = t;
                 count++;
                 printLine();
-                System.out.println(" added: " + user_input);
+                System.out.println(" added: " + userInput);
                 printLine();
             }
         }
@@ -35,5 +40,35 @@ public class Junny {
 
     public static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void printAllTasks(Task[] tasks, int count) {
+        printLine();
+        System.out.println("Here are the tasks in your list: ");
+        for (int i = 0; i < count; i++) {
+            System.out.println(tasks[i].toString());
+        }
+        printLine();
+    }
+
+    public static void printBye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void markDone(Task task) {
+        printLine();
+        System.out.println("Nice! I've marked this task as done:");
+        task.markAsDone();
+        System.out.println(task.toString());
+        printLine();
+    }
+
+    public static void markUndone(Task task) {
+        printLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        task.markAsNotDone();
+        System.out.println(task.toString());
+        printLine();
     }
 }
