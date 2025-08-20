7969c96e98f1a99561816182a71a0581c089e4c1
junrong <E1354441@u.nus.edu>
2025-08-20 23:20:37 +0800

    Level-1: Echo

--- src/main/java/Junny.java
+++ src/main/java/Junny.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Junny {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Junny");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String user_input = scanner.nextLine();
+            if (user_input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(user_input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
