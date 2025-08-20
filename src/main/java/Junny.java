e251268382bc369ed0999ff46e5a89d18d1d2ff0
junrong <E1354441@u.nus.edu>
2025-08-20 23:40:27 +0800

    Level-2: Add, List

--- src/main/java/Junny.java
+++ src/main/java/Junny.java
@@ -2,24 +2,38 @@ import java.util.Scanner;
 
 public class Junny {
     public static void main(String[] args) {
+        String[] list_of_act = new String[100];
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println(" Hello! I'm Junny");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        printLine();
 
         while (true) {
             String user_input = scanner.nextLine();
             if (user_input.equals("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                printLine();
                 break;
+            } else if (user_input.equals("list")) {
+                printLine();
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + "." + list_of_act[i]);
+                }
+                printLine();
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println(user_input);
-                System.out.println("____________________________________________________________");
+                list_of_act[count] = user_input;
+                count++;
+                printLine();
+                System.out.println(" added: " + user_input);
+                printLine();
             }
         }
         scanner.close();
+    }
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
